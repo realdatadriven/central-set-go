da9a75e8c7157cb95f8e2eda97531bf8596d2b0d
Real-Datadriven <real.datadriven@gmail.com>
2025-07-04 14:02:59 -0100

    ui update

--- cmd/api/query.go
+++ cmd/api/query.go
@@ -7,12 +7,12 @@ import (
 	"github.com/realdatadriven/etlx"
 )
 
-func (app *application) CrudRunQuery(params map[string]interface{}, query string, db etlx.DBInterface) map[string]interface{} {
+func (app *application) CrudRunQuery(params map[string]any, query string, db etlx.DBInterface) map[string]any {
 	patt := regexp.MustCompile(`CREATE.*TABLE|UPDATE.*TABLE|DROP.*|INSERT.*INTO|DELETE|ALTER.*TABLE|UPSERT.*`)
 	_match := patt.FindAllString(query, -1)
 	if len(_match) > 0 {
-		msg, _ := app.i18n.T("query-not-allowed", map[string]interface{}{"query": query, "match": app.joinSlice(app.sliceStrs2SliceInterfaces(_match), ";")})
-		return map[string]interface{}{
+		msg, _ := app.i18n.T("query-not-allowed", map[string]any{"query": query, "match": app.joinSlice(app.sliceStrs2SliceInterfaces(_match), ";")})
+		return map[string]any{
 			"success": false,
 			"msg":     msg,
 		}
@@ -22,36 +22,36 @@ func (app *application) CrudRunQuery(params map[string]interface{}, query string
 	_match = patt.FindAllString(query, -1)
 	if len(_match) == 0 {
 		limit := 10
-		if _, ok := params["data"].(map[string]interface{})["limit"]; ok {
-			limit = int(params["data"].(map[string]interface{})["limit"].(float64))
+		if _, ok := params["data"].(map[string]any)["limit"]; ok {
+			limit = int(params["data"].(map[string]any)["limit"].(float64))
 		}
 		offset := 0
-		if _, ok := params["data"].(map[string]interface{})["offset"]; ok {
-			offset = int(params["data"].(map[string]interface{})["offset"].(float64))
+		if _, ok := params["data"].(map[string]any)["offset"]; ok {
+			offset = int(params["data"].(map[string]any)["offset"].(float64))
 		}
 		if limit != -1 {
 			query = fmt.Sprintf(`%s LIMIT %d OFFSET %d`, query, limit, offset)
 		}
 	}
-	results, cols, _, err := db.QueryMultiRowsWithCols(query, []interface{}{}...)
+	results, cols, _, err := db.QueryMultiRowsWithCols(query, []any{}...)
 	//fmt.Println(query, (*results))
 	if err != nil {
-		return map[string]interface{}{
+		return map[string]any{
 			"success": false,
 			"msg":     fmt.Sprintf("%s", err),
 		}
 	}
-	n_rows, _, err := db.QuerySingleRow(query_n_rows, []interface{}{}...)
+	n_rows, _, err := db.QuerySingleRow(query_n_rows, []any{}...)
 	if err != nil {
-		return map[string]interface{}{
+		return map[string]any{
 			"success": false,
 			"msg":     fmt.Sprintf("%s", err),
 		}
 	}
 	total := 0
 	total = int((*n_rows)["n_rows"].(int64))
-	msg, _ := app.i18n.T("success", map[string]interface{}{})
-	return map[string]interface{}{
+	msg, _ := app.i18n.T("success", map[string]any{})
+	return map[string]any{
 		"success": true,
 		"msg":     msg,
 		"sql":     query,
