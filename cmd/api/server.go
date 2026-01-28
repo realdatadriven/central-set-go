1f2263f1a694876b26cde9b10a4fdc91edf6bd55
realdatadriven <134167653+realdatadriven@users.noreply.github.com>
2026-01-28 16:51:46 -0100

    fixes

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -27,7 +27,6 @@ const (
 )
 
 func (app *application) serveHTTP() error {
-
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.httpPort),
 		Handler:      app.routes(),
@@ -36,9 +35,7 @@ func (app *application) serveHTTP() error {
 		ReadTimeout:  defaultReadTimeout,
 		WriteTimeout: defaultWriteTimeout,
 	}
-
 	shutdownErrorChan := make(chan error)
-
 	go func() {
 		quitChan := make(chan os.Signal, 1)
 		signal.Notify(quitChan, syscall.SIGINT, syscall.SIGTERM)
@@ -49,19 +46,16 @@ func (app *application) serveHTTP() error {
 
 		shutdownErrorChan <- srv.Shutdown(ctx)
 	}()
-
-	app.logger.Info("starting server", slog.Group("server", "addr", srv.Addr))
-	
+	app.logger.Info("starting server", slog.Group("server", "addr", srv.Addr))	
+	enableTLS := strings.ToLower(os.Getenv("ENABLE_TLS")) == "true"
 	if enableTLS {
 		certFile := os.Getenv("TLS_CERT_FILE")
 		keyFile := os.Getenv("TLS_KEY_FILE")
-
 		if certFile == "" || keyFile == "" {
 			app.logger.Error("ENABLE_TLS is true but TLS_CERT_FILE or TLS_KEY_FILE is not set")
 		}
-
 		app.logger.Info("🔐 Starting HTTPS server on ", slog.Group("server", "addr", srv.Addr))
-		err := srv.ListenAndServeTLS()
+		err := srv.ListenAndServeTLS(certFile, keyFile)
 		if !errors.Is(err, http.ErrServerClosed) {
 			return err
 		}
@@ -71,14 +65,11 @@ func (app *application) serveHTTP() error {
 			return err
 		}
 	}
-
 	err = <-shutdownErrorChan
 	if err != nil {
 		return err
 	}
-
 	app.logger.Info("stopped server", slog.Group("server", "addr", srv.Addr))
-
 	app.wg.Wait()
 	return nil
 }
