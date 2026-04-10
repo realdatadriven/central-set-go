9822f1be27dbaf595a802f61927558384ab73b64
realdatadriven <134167653+realdatadriven@users.noreply.github.com>
2026-04-10 12:31:08 -0100

    updaes

--- cmd/api/etlx.go
+++ cmd/api/etlx.go
@@ -296,7 +296,7 @@ C AS (
         , yaml_extract(metadata::YAML, '$.name') as metadata_name
         , yaml_extract(metadata::YAML, '$.description') as metadata_description
         , yaml_extract(metadata::YAML, '$.source') as metadata_source
-        , coalesce(yaml_extract(metadata::YAML, '$.runs_as'), case when level = 1 then metadata_name else null end) as runs_as
+        , coalesce(yaml_extract(metadata::YAML, '$.runs_as'), case when level = 1 then /*metadata_name*/ null else null end) as runs_as
         , yaml_extract(metadata::YAML, '$.depends_on') as metadata_depends_on
         , yaml_type(metadata_depends_on) as _type_depends_on
         , yaml_exists(metadata::YAML, '$.depends_on') as _exists_depends_on
@@ -311,11 +311,31 @@ C AS (
             when level = 1 then row_number() over (partition by level order by row)
             else row_number() over (partition by level, parent_id order by row)
         end as level_order
-    from A
+        -- check with regex if content has [[query_name]] references to other sections. We can use this to build a dependency graph and lineage.
+        -- extract only query_name from [[query_name]]
+        , regexp_extract_all(content, '\\[\\[(\w+)\\]\\]') as refered_queries
+    from A 
 ),
 D AS (
     select C.*, UNNEST(C.others).language as language, UNNEST(C.others).code as code, UNNEST(C.others).info_string as info_string
     from C
+),
+Q AS (
+    /*GET THE CONTENT FROM THE QUERY_DOC REFFERECED IN THE SECTION CONTENT*/
+    WITH X AS (
+        SELECT C.*, Parent.title as parent_title
+        FROM C    
+        LEFT OUTER JOIN C AS Parent ON Parent.section_id = C.parent_id
+    )
+    select X.section_id, X.refered_queries
+        , ARRAY_AGG(Q.section_id) as refered_query_section_ids
+        , GROUP_CONCAT(DISTINCT Q.content) as refered_query_content
+    from X
+    JOIN X AS Q ON
+        CAST(X.refered_queries AS TEXT) LIKE CONCAT('%', Q.title, '%')
+        OR CAST(X.refered_queries AS TEXT) LIKE CONCAT('%', Q.parent_title, '%')
+    WHERE LEN(X.refered_queries) > 0
+    GROUP BY X.section_id, X.refered_queries
 )
 select C.*/*, D.language, D.code, D.info_string
     , case
@@ -327,8 +347,11 @@ select C.*/*, D.language, D.code, D.info_string
     , parent.level as parent_level
     , parent.metadata_name as parent_metadata_name
     , parent.runs_as as parent_runs_as
+    , Q.refered_query_section_ids
+    , Q.refered_query_content
 from C
 LEFT OUTER JOIN C AS Parent ON Parent.section_id = C.parent_id
+LEFT OUTER JOIN Q ON Q.section_id = C.section_id
 /*LEFT OUTER JOIN D ON D.section_id = C.section_id*/
 order by C.row;`
 	query = etlxlib.ReplaceFileTablePlaceholder("file", query, fname)
@@ -399,6 +422,9 @@ LEFT OUTER JOIN markdown_sections AS B ON
 	INSTR(A.content::VARCHAR, B.parent_title::VARCHAR || '.' || B.title::VARCHAR) > 0
 	OR INSTR(A.content::VARCHAR, B.parent_metadata_name::VARCHAR || '.' || B.metadata_name::VARCHAR) > 0
 	OR INSTR(A.content::VARCHAR, B.metadata_name::VARCHAR) > 0
+    OR INSTR(A.refered_query_content::VARCHAR, B.parent_title::VARCHAR || '.' || B.title::VARCHAR) > 0
+    OR INSTR(A.refered_query_content::VARCHAR, B.parent_metadata_name::VARCHAR || '.' || B.metadata_name::VARCHAR) > 0
+    OR INSTR(A.refered_query_content::VARCHAR, B.metadata_name::VARCHAR) > 0
 OR INSTR(A.content::VARCHAR, B.metadata_name::VARCHAR) > 0
 WHERE B.section_id IS NOT NULL
 	AND B.section_id != A.section_id
