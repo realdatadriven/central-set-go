1ebc8da067aaf22c6d69af928ff3142dca9d1d8c
realdatadriven <134167653+realdatadriven@users.noreply.github.com>
2026-06-17 11:01:24 +0000

    feat: add functions to generate PDF from LaTeX and escape special characters

--- cmd/api/pdf.go
+++ cmd/api/pdf.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
@@ -27,6 +30,57 @@ func (app *application) GenPDFFromHTML(html, output_path string) error {
 		return err
 	}
 	err = os.WriteFile(output_path, pdf, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LatexEscape(v any) string {
+	s := fmt.Sprint(v)
+
+	replacer := strings.NewReplacer(
+		"\\", "\\textbackslash{}",
+		"&", "\\&",
+		"%", "\\%",
+		"$", "\\$",
+		"#", "\\#",
+		"_", "\\_",
+		"{", "\\{",
+		"}", "\\}",
+		"~", "\\textasciitilde{}",
+		"^", "\\textasciicircum{}",
+	)
+
+	return replacer.Replace(s)
+}
+
+func (app *application) GenPDFFromLatex(latex, output_path string) error {
+	latex = LatexEscape(latex)
+	//ctx, cancel := chromedp.NewContext(context.Background())
+	//defer cancel()
+	temptex, err := os.CreateTemp("", "*.tex")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(temptex.Name())
+	defer temptex.Close()
+
+	_, err = temptex.WriteString(latex)
+	if err != nil {
+		return err
+	}
+	temptex.Close()
+
+	cmd := exec.Command("pdflatex", temptex.Name())
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	// Move generated PDF to output path
+	tempPDF := strings.TrimSuffix(temptex.Name(), ".tex") + ".pdf"
+	err = os.Rename(tempPDF, output_path)
 	if err != nil {
 		return err
 	}
