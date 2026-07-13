40b3ff23a0f87b9d4ada47d9465fc53654edbabd
realdatadriven <134167653+realdatadriven@users.noreply.github.com>
2026-07-13 16:02:02 -0100

    fixes

--- cmd/api/ssh.go
+++ cmd/api/ssh.go
@@ -10,6 +10,13 @@ import (
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/knownhosts"
+
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+
+	"golang.org/x/crypto/ssh"
 )
 
 type Runner struct {
@@ -270,4 +277,84 @@ ls -la
 	if err != nil {
 		log.Fatal(err)
 	}
+*/
+
+type Credentials struct {
+	PrivateKeyPEM string // PKCS#8 PEM
+	PublicKeySSH  string // OpenSSH authorized_keys format
+}
+
+func GenerateCredentials() (*Credentials, error) {
+	// Generate Ed25519 key pair.
+	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	// Encode the private key as PKCS#8.
+	pkcs8, err := x509.MarshalPKCS8PrivateKey(priv)
+	if err != nil {
+		return nil, err
+	}
+	privatePEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: pkcs8,
+	})
+	// Encode the public key in OpenSSH format.
+	pubKey, err := ssh.NewPublicKey(pub)
+	if err != nil {
+		return nil, err
+	}
+	publicSSH := ssh.MarshalAuthorizedKey(pubKey)
+	return &Credentials{
+		PrivateKeyPEM: string(privatePEM),
+		PublicKeySSH:  string(publicSSH),
+	}, nil
+}
+
+func AddKnownHost(host string, port int, knownHosts string) error {
+	cmd := exec.Command(
+		"ssh-keyscan",
+		"-H",
+		"-p",
+		fmt.Sprint(port),
+		host,
+	)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	f, err := os.OpenFile(
+		knownHosts,
+		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
+		0600,
+	)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(out.Bytes())
+	return err
+}
+
+/*cred, err := sshutil.GenerateCredentials()
+if err != nil {
+	log.Fatal(err)
+}
+
+fmt.Println("PRIVATE KEY:")
+fmt.Println(cred.PrivateKeyPEM)
+
+fmt.Println("PUBLIC KEY:")
+fmt.Println(cred.PublicKeySSH)
+
+err = sshutil.AddKnownHost(
+	"csete.online",
+	22,
+	"./deployment/demo/known_hosts",
+)
+if err != nil {
+	log.Fatal(err)
+}
 */
