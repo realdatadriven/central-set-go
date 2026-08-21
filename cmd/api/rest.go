9d18ec8eabc6ca1ff102cb68d48e67bf70a6cc08
realdatadriven <134167653+realdatadriven@users.noreply.github.com>
2026-08-21 09:37:44 +0000

    fixes

--- cmd/api/rest.go
+++ cmd/api/rest.go
@@ -71,8 +71,8 @@ func (app *application) crudPrimaryKey(params Dict, db, table string) (string, e
 }
 
 func (app *application) getPKFromSchema(db, table string) (string, error) {
-	sql := `select field as column_name from table_schema where db = ? and "table" = ? and pk = true`
-	pk, err := app.AdminGetRowByFilter(sql, []any{db, table})
+	sql := `select field as column_name from table_schema where lower(db) = ? and "table" = ? and pk = true`
+	pk, err := app.AdminGetRowByFilter(sql, []any{strings.ToLower(db), table})
 	if err != nil {
 		return "", fmt.Errorf("failed to query for PK Field %s %s: %w", db, table, err)
 	}
